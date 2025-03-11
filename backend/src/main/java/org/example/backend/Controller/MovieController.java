09a98f366a29a901a1172d41e65fc2204db07f95
Ramez Chreide <ramiz_chreide@outlook.com>
2025-03-11 20:43:01 +0100

    add: getMovies, getMovie

--- backend/src/main/java/org/example/backend/Controller/MovieController.java
+++ backend/src/main/java/org/example/backend/Controller/MovieController.java
@@ -1,9 +1,16 @@
 package org.example.backend.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.Data.MovieData;
 import org.example.backend.Service.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/movie")
@@ -13,4 +20,18 @@ public class MovieController
     private final MovieService movieService;
 
     //todo implement stuff
+
+    @GetMapping
+    public List<MovieData> getMovies() {
+        return movieService.getMovies();
+    }
+
+    @GetMapping("{id}")
+    public MovieData getMovie(@PathVariable String id) {
+        Optional<MovieData> movie = movieService.getMovie(id);
+        if(movie.isPresent()) {
+            return movie.get();
+        }
+        throw new NoSuchElementException("Movie with ID: "+ id + " not found");
+    }
 }
