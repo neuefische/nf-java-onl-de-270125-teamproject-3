85c637537e3dd04e75132b6eda6b04062f8c0334
vuqu-git <vuqu@fantasymail.de>
2025-03-12 15:27:27 +0100

    service method updateTodo: created + unit tested

--- backend/src/main/java/org/example/backend/Service/MovieService.java
+++ backend/src/main/java/org/example/backend/Service/MovieService.java
@@ -6,6 +6,7 @@ import org.example.backend.Data.MovieRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -21,4 +22,20 @@ public class MovieService
     public Optional<MovieData> getMovie(String id) {
         return movieRepo.findById(id);
     }
-}
+
+    public MovieData updateMovie(String targetId, MovieData updatedMovie) {
+
+        if (!movieRepo.existsById(targetId)) {
+            throw new NoSuchElementException(String.format("No movie found with the id %s", targetId));
+        }
+
+        // Ensure the id in the updatedMovie matches the path variable id.
+        if (!targetId.equals(updatedMovie.id())) {
+            throw new IllegalArgumentException("ID in path and body do not match");
+        }
+        return movieRepo.save(updatedMovie);
+    }
+
+    }
+
+
