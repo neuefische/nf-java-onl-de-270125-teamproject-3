23e1b7073ac663b8f551eeee7578cc3222ac002b
Ramez Chreide <ramiz_chreide@outlook.com>
2025-03-12 15:09:50 +0100

    add: axios getMovies, getMovie

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
+import './App.css';
+
+interface Movie {
+    id: string;
+    title: string;
+    director: string;
+    releaseYear: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [movie, setMovie] = useState<Movie>();
+
+    const baseURL = "/api/movie"
+
+    const getMovies = () => {
+        console.log("Fetching Movies...")
+
+        axios.get(baseURL)
+            .then((response) => {
+                console.log("Request finished")
+                console.log(response.data)
+                setMovies(response.data)
+            })
+            .catch((errorResponse) => {
+                console.log(errorResponse)
+            })
+
+        console.log("Movies fetched successfully!")
+    }
+
+    const getMovie = (id:string) => {
+        console.log(`Fetching Movie with ${id}...`)
+
+        axios.get(`${baseURL}/${id}`)
+            .then((response) => {
+                console.log("Request finished")
+                console.log(response.data)
+                setMovie(response.data)
+            })
+            .catch((errorResponse) => {
+                console.log(errorResponse)
+            })
+
+        console.log("Movie fetched successfully!")
+    }
+
+    useEffect(() => {
+        getMovies();
+        getMovie("67d1702820ade63ce409e544")
+    }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    movies.map((movie) => <div key={movie.id}>
+        <h3>{movie.title}</h3>
+        <p>{movie.director}</p>
+        <p>{movie.releaseYear}</p>
+    </div>)
   )
 }
 
